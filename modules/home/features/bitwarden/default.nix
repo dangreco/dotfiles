e04a6c314af76c57e47667c58d11a51e3bec2d58
Dan Greco <git@dangre.co>
2026-07-11 18:19:28 -0400

    feat: add bitwarden ssh agent socket

--- modules/home/features/bitwarden/default.nix
+++ modules/home/features/bitwarden/default.nix
@@ -15,5 +15,9 @@ in
       bitwarden-cli
       bitwarden-desktop
     ];
+
+    home.sessionVariables = {
+      SSH_AUTH_SOCK = "${config.home.homeDirectory}/.bitwarden-ssh-agent.sock";
+    };
   };
 }
